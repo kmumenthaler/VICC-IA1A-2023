035fbb28036b6cad8489c40814d1c2d24f4ac02b
root <root@VICC-IA1A.5mzk1bh5ya4ulchpcw4akonp4b.ax.internal.cloudapp.net>
2023-09-06 21:56:25 +0000

    Neuen Branch erstellt.

---
+++ app/config.py
@@ -0,0 +1,15 @@
+
+# Database configuration
+DB_CONFIG = {
+    'host': 'db',
+    'user': 'root',
+    'password': 'Start123.',
+    'database': 'BuchrezensionsPlattform'
+}
+
+# Flask Secret Key
+SECRET_KEY = '1576bfab8ae6bda607dd03c271afe64a'
+
+# Weitere Configs
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = './static/img/books/'
